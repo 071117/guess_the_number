c77954d588bb6f830ff8416ce466f442a03ef1ec
Гамзат Мамаев <mamaevvgamzatt@gmail.com>
2025-01-10 23:43:13 +0300

    Я доработал код, теперь в игре возможно: Подсчет попыток, сделанных пользователем. Когда число отгадано, программа должна показать количество попыток; Возможность генерации нового числа (повторная игра), после того, как пользователь угадал число; Добавить возможность указания правой границы для случайного выбора числа (от 1 до n).

--- .venv/main.py
+++ .venv/main.py
@@ -1,41 +1,77 @@
-from random import *
-x = randint(1, 100)
-a = ['Слишком много, попробуйте еще раз', 'Слишком мало, попробуйте еще раз', 'Вы угадали, поздравляем!']
-print('Добро пожаловать в числовую угадайку')
-print('Введи число!')
-digit = int(input())
+from random import randint
+import math
 
-flag = True
-
-def is_valid(z):
-    return 1 <= z <= 100
-
-while not is_valid(digit):
-    print('А может быть все-таки введем целое число от 1 до 100?')
-    digit = int(input())
+# Глобальные переменные
+n = 100  # Максимально возможное загадываемое число
+sm = 0  # Счетчик попыток
+best_result = 0  # Лучший результат
 
 
-def guess(digit):
+# Используемые функции
+def is_valid(z):  # Защита от дурака
+    return 1 <= z <= n
+
+
+def guess(digit, x):
+    global sm
     while digit != x:
         if digit > x:
-            print(a[0])
+            print('Слишком много, попробуйте еще раз')
         elif digit < x:
-            print(a[1])
+            print('Слишком мало, попробуйте еще раз')
 
+        sm += 1  # Увеличиваем счетчик попыток
         digit = int(input())
 
         while not is_valid(digit):
             print('А может быть все-таки введем целое число от 1 до 100?')
             digit = int(input())
 
-    print(a[2])
+    print('Вы угадали, поздравляем!')
+
+print('Добро пожаловать в числовую угадайку')
+
+def fun():
+    global n, best_result
+    print('Хотите изменить максимально возможное загадываемое число? По умолчанию стоит число 100.')
+    question = input()
+    if question.capitalize() == 'Да' or question.capitalize() == 'Yes':
+        n = int(input())
+
+    print('Введи число!')
+    digit = int(input())
+
+    while not is_valid(digit):
+        print('А может быть все-таки введем целое число от 1 до ', n, '?', sep='')
+        digit = int(input())
+
+    x = randint(1, n)  # Загадываемое число
+    guess(digit, x)
+
+    best_result = math.ceil(math.log2(n))
 
 
-guess(digit)
+fun()
+
+print('Хотите узнать сколько Вы использовали попыток, чтобы угадать число?:)')
+question = input()
+if question.capitalize() == 'Да' or question.capitalize() == 'Yes':
+    print(f'Вы потратили {sm + 1} попыток ;)'
+          f'Лучший, возможный, результат {best_result}!')
+flag = True
+while flag == True:
+    print('Хотите сыграть еще? Это бесплатно!')
+    question = input()
+    if question.capitalize() == 'Да' or question.capitalize() == 'Yes':
+        sm = 0  # Сброс счетчика попыток
+        n = 100
+        fun()
+        print('Хотите узнать сколько Вы использовали попыток, чтобы угадать число?:)')
+        question = input()
+        if question.capitalize() == 'Да' or question.capitalize() == 'Yes':
+            print(f'Вы потратили {sm + 1} попыток ;)'
+                  f'Лучший, возможный, результат {best_result}!')
+    else:
+        flag = False
+
 print('Спасибо, что играли в числовую угадайку. Еще увидимся...')
-
-
-
-
-
-
