dec341e58845664d443c327bb01d5114e38c2bb8
Гамзат Мамаев <mamaevvgamzatt@gmail.com>
2025-01-10 22:44:45 +0300

    Я написал код для бота, числовая угадайка. Зачем? Чтобы стать программистом!

--- .venv/main.py
+++ .venv/main.py
@@ -1,7 +1,41 @@
-print('Hellooooooooooooo')
-print('чтото еще')
-print('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA')
-ОШИБКА
-ЫВРШИАМА
-надо НАЧАТЬ ЗАНОГО ПИСАТЬ КОД
-ОООЫТЗЫТМЦКЗМ
+from random import *
+x = randint(1, 100)
+a = ['Слишком много, попробуйте еще раз', 'Слишком мало, попробуйте еще раз', 'Вы угадали, поздравляем!']
+print('Добро пожаловать в числовую угадайку')
+print('Введи число!')
+digit = int(input())
+
+flag = True
+
+def is_valid(z):
+    return 1 <= z <= 100
+
+while not is_valid(digit):
+    print('А может быть все-таки введем целое число от 1 до 100?')
+    digit = int(input())
+
+
+def guess(digit):
+    while digit != x:
+        if digit > x:
+            print(a[0])
+        elif digit < x:
+            print(a[1])
+
+        digit = int(input())
+
+        while not is_valid(digit):
+            print('А может быть все-таки введем целое число от 1 до 100?')
+            digit = int(input())
+
+    print(a[2])
+
+
+guess(digit)
+print('Спасибо, что играли в числовую угадайку. Еще увидимся...')
+
+
+
+
+
+
